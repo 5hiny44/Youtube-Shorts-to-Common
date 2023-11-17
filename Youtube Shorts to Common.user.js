0a775f681adfd8bf92880a17e3f385ea4c804ac7
Xavier Hsiao <x6714h2@yahoo.com.tw>
2023-11-18 02:01:43 +0800

    Modify code logic.
    Modify the logic for identifying shortsID
    Fixed the navigation issue when clicking on the shorts category of the channel.

--- Youtube Shorts to Common.user.js
+++ Youtube Shorts to Common.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Shorts to Common
 // @namespace    https://github.com/5hiny44/Youtube-Shorts-to-Common
-// @version      1.1
+// @version      1.2.0
 // @encoding     utf-8
 // @description  When you click on the 'Youtube Shorts' page, a script will be automatically called to redirect you to the common version of the page.
 // @author       Xavier Hsiao
@@ -12,8 +12,9 @@
 // @updateURL    https://raw.githubusercontent.com/5hiny44/Youtube-Shorts-to-Common/main/Youtube%20Shorts%20to%20Common.user.js
 // ==/UserScript==
 
-// 1.打開shorts頁面時，能自動跳轉至一般頁面。
-// 2.觀看完後，上一頁回到shorts頁面，能避免反覆跳轉。(利用 localStorage 來記住最後一次瀏覽的影片ID是什麼。)
+// 目標：
+//   1.打開shorts頁面時，能自動跳轉至一般頁面。
+//   2.觀看完後，上一頁能回到shorts頁面，能避免反覆跳轉。(利用 localStorage 來記住最後一次瀏覽的影片ID是什麼。)
 
 (function() {
     'use strict';
@@ -29,21 +30,18 @@
     function shortsToCommon() {
         const currentURL = window.location.href;
         const urlWithoutQueryString = currentURL.split("?")[0];
-        const queryString = currentURL.split("?")[1];
-
-        if(urlWithoutQueryString.match("/watch")) {
-            localStorage.setItem("previousVideoID", queryString.match(/v=[^?&#/]*/g)[0].split("=")[1]);
-            return 0;
-        };
 
         if(urlWithoutQueryString.match("/shorts")) {
-            const urlElements = urlWithoutQueryString.split("/");
-            const shortsID = urlElements[urlElements.length - 1];
+            
+            // shorts category of channel. ex. https://www.youtube.com/@GoodChennel/shorts
+            // shorts video. ex. https://www.youtube.com/shorts/20oooxxxx?si=aaabbbbccc
+            const shortsID = urlWithoutQueryString.match(/\/shorts\/[^?&#/]*/g)[0].split("/")[2];
 
             if(shortsID == localStorage.getItem("previousVideoID")) {
                 // 如果前次預覽過這支影片，則回到 shorts 就不要再次跳轉。
                 return 0;
             };
+            localStorage.setItem("previousVideoID", shortsID);
 
             const youtubeURL = "https://www.youtube.com";
             const uri = "/watch?v=" + shortsID;
